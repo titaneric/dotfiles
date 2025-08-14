a8e52b8bda19c2db575ecc29a4bb4d6a34a48ec7
titaneric <chenyihuang001@gmail.com>
2025-08-14 23:24:22 +0800

    init commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+history merge
+
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+set -U fish_user_paths $HOME/.krew/bin $fish_user_paths
+set -U fish_user_paths $HOME/go/bin $fish_user_paths
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -U fish_user_paths $HOME/.bin $fish_user_paths
+
+if status is-interactive
+  set -g fish_key_bindings fish_vi_key_bindings
+  
+  starship init fish | source
+  atuin init fish | source
+  direnv hook fish | source
+  kubectl completion fish | source
+  zoxide init fish | source
+end
