26b083cd1d37d6bdf2b1729967db8bd000ff9442
titaneric <chenyihuang001@gmail.com>
2025-08-21 02:19:02 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,6 +5,7 @@ set -U fish_user_paths $HOME/.krew/bin $fish_user_paths
 set -U fish_user_paths $HOME/go/bin $fish_user_paths
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set -U fish_user_paths $HOME/.bin $fish_user_paths
+set -U fish_user_paths $HOME/.bun/bin $fish_user_paths
 
 if status is-interactive
   set -g fish_key_bindings fish_vi_key_bindings
@@ -15,3 +16,7 @@ if status is-interactive
   kubectl completion fish | source
   zoxide init fish | source
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
