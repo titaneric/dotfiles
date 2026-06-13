320467403db7600b4f19a85e346d2490dc53df00
titaneric <chenyihuang001@gmail.com>
2026-06-14 01:10:10 +0800

    install playwright-cli

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ history merge
 fish_add_path -U /opt/homebrew/bin $HOME/.npm-global/bin $HOME/.krew/bin $HOME/go/bin $HOME/.cargo/bin
 
 set -gx GPG_TTY (tty)
+set -gx PLAYWRIGHT_MCP_BROWSER msedge
 
 if status is-interactive
   set -g fish_key_bindings fish_vi_key_bindings
