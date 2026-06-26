888f5e11242cfd0ec9cdb4c5c5d405ae899f2a8f
titaneric <chenyihuang001@gmail.com>
2026-06-26 22:22:29 +0800

    Update Brewfile Update .agents/.skill-lock.json Update .config/fish/config.fish Update .gitconfig

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,9 +1,13 @@
 history merge
 
-fish_add_path -U /opt/homebrew/bin $HOME/.local/bin $HOME/.npm-global/bin $HOME/.krew/bin $HOME/go/bin $HOME/.cargo/bin
+fish_add_path -U /opt/homebrew/bin $HOME/.local/bin $HOME/.npm-global/bin $HOME/.krew/bin $HOME/go/bin $HOME/.cargo/bin /opt/homebrew/opt/openssl@4/bin
 
 set -gx GPG_TTY (tty)
 set -gx PLAYWRIGHT_MCP_BROWSER msedge
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/openssl@4/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@4/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@4/lib/pkgconfig"
 
 if status is-interactive
   set -g fish_key_bindings fish_vi_key_bindings
